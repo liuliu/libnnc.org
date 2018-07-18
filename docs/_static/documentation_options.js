cc426cf691a0684d3d6200db41d8bb297f8faed4
Liu Liu <i@liuliu.me>
2018-07-18 01:47:41 -0400

    Add new docs.

---
+++ docs/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'a deep learning framework from ccv',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
