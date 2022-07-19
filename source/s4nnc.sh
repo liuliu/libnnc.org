b2ebd53b77dfafa3a135b1c91d787d8355ab5573
Liu Liu <i@liuliu.me>
2022-07-19 00:45:08 -0400

    Update docs.

--- source/s4nnc.sh
+++ source/s4nnc.sh
@@ -8,6 +8,10 @@ for i in ../../s4nnc/nnc/*.swift ; do
     ../../SourceKitten/.build/release/sourcekitten doc --single-file $i -- ../../s4nnc/nnc/*.swift > _s4nnc/$(basename $i .swift).json
 done
 
+for i in $(find ../../s4nnc/gym -name '*.swift') ; do
+    ../../SourceKitten/.build/release/sourcekitten doc --single-file $i -- ../../s4nnc/gym/*.swift ../../s4nnc/gym/envs/*.swift ../../s4nnc/gym/policies/*.swift ../../s4nnc/gym/renders/*.swift > _s4nnc/$(basename $i .swift).json
+done
+
 cp ../../s4nnc/README.md  _s4nnc/
 
 cd _s4nnc
