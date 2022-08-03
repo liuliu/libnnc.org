4b60e6ac0e97ca3cf6e6b519f6dca7656835bbd6
Liu Liu <i@liuliu.me>
2022-08-03 13:03:14 -0400

    Update with new APIs.

--- source/s4nnc.sh
+++ source/s4nnc.sh
@@ -9,7 +9,11 @@ for i in ../../s4nnc/nnc/*.swift ; do
 done
 
 for i in $(find ../../s4nnc/gym -name '*.swift') ; do
-    ../../SourceKitten/.build/release/sourcekitten doc --single-file $i -- ../../s4nnc/gym/*.swift ../../s4nnc/gym/envs/*.swift ../../s4nnc/gym/policies/*.swift ../../s4nnc/gym/renders/*.swift > _s4nnc/$(basename $i .swift).json
+    ../../SourceKitten/.build/release/sourcekitten doc --single-file $i -- ../../s4nnc/gym/*.swift ../../s4nnc/gym/envs/*.swift ../../s4nnc/gym/policies/*.swift ../../s4nnc/gym/renders/*.swift ../../s4nnc/gym/http/*.swift > _s4nnc/$(basename $i .swift).json
+done
+
+for i in $(find ../../s4nnc/tensorboard -name '*.swift') ; do
+    ../../SourceKitten/.build/release/sourcekitten doc --single-file $i -- ../../s4nnc/tensorboard/*.swift ../../s4nnc/tensorboard/proto-generated/*.swift > _s4nnc/$(basename $i .swift).json
 done
 
 cp ../../s4nnc/README.md  _s4nnc/
